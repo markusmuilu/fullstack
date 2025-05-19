f3a5022d19ed2c1f1c107bf632878a951f8bcc81
Markus <markus.muilu@gmail.com>
2025-05-19 22:09:19 +0300

    Refactor 	modified:   kurssitiedot/src/App.jsx 	renamed:    part1/.gitignore -> osa1/counter/.gitignore 	renamed:    part1/README.md -> osa1/counter/README.md 	renamed:    part1/eslint.config.js -> osa1/counter/eslint.config.js 	renamed:    part1/index.html -> osa1/counter/index.html 	new file:   osa1/counter/package-lock.json 	new file:   osa1/counter/package.json 	renamed:    part1/public/vite.svg -> osa1/counter/public/vite.svg 	renamed:    part1/src/App.css -> osa1/counter/src/App.css 	new file:   osa1/counter/src/App.jsx 	renamed:    part1/src/assets/react.svg -> osa1/counter/src/assets/react.svg 	renamed:    part1/src/index.css -> osa1/counter/src/index.css 	new file:   osa1/counter/src/main.jsx 	renamed:    part1/vite.config.js -> osa1/counter/vite.config.js 	new file:   osa1/part1/.gitignore 	new file:   osa1/part1/README.md 	new file:   osa1/part1/eslint.config.js 	new file:   osa1/part1/index.html 	renamed:    part1/package-lock.json -> osa1/part1/package-lock.json 	renamed:    part1/package.json -> osa1/part1/package.json 	new file:   osa1/part1/public/vite.svg 	new file:   osa1/part1/src/App.css 	renamed:    part1/src/App.jsx -> osa1/part1/src/App.jsx 	new file:   osa1/part1/src/assets/react.svg 	new file:   osa1/part1/src/index.css 	renamed:    part1/src/main.jsx -> osa1/part1/src/main.jsx 	new file:   osa1/part1/vite.config.js

--- osa1/part1/src/App.jsx
+++ osa1/part1/src/App.jsx
@@ -1,12 +1,16 @@
-const Hello = (props) => {
+const Hello = ({name, age}) => {
+  const bornYear = () => new Date().getFullYear() - age
+  
   return (
     <div>
       <p>
-        Hello {props.name}, you are {props.age} years old
+        Hello {name}, you are {age} years old
       </p>
+      <p>So you were probably born {bornYear()}</p>
     </div>
   )
- }
+}
+ 
 const App = () => {
   const nimi = 'Pekka'
   const ika = 10
