71e7a025bb39609116662b33d9712af5bc6c331b
markusmuilu <muilu.markus@gmail.com>
2025-07-19 20:31:51 +0300

    3.15-18

--- osa3/notebackend/index.js
+++ osa3/notebackend/index.js
@@ -6,44 +6,59 @@ const app = express()
 app.use(express.json())
 app.use(express.static('dist'))
 
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
-
 app.get('/api/notes', (request, response) => {
     Note.find({}).then(notes => {
       response.json(notes)
     })
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
     Note.findById(request.params.id).then(note => {
-      response.json(note)
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
     })
+    .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response) => {
     const id = request.params.id
-    notes = notes.filter(note => note.id !== id)
+    Note.findByIdAndDelete(id)
+    .then(note => {
+      if (!note) {
+        return response.status(404).end()
+      }
 
-    response.status(204).end()
+      note.content = content
+      note.important = important
+
+      return note.save().then((updatedNote) => {
+        response.json(updatedNote)
+      })
+    })
+    .catch(error => next(error))
 })
 
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body
 
+  Note.findById(request.params.id)
+    .then(note => {
+      if (!note) {
+        return response.status(404).end()
+      }
+
+      note.content = content
+      note.important = important
+
+      return note.save().then((updatedNote) => {
+        response.json(updatedNote)
+      })
+    })
+    .catch(error => next(error))
+})
 
 app.post('/api/notes', (request, response) => {
     const body = request.body
@@ -64,7 +79,43 @@ app.post('/api/notes', (request, response) => {
     })
 })
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
 const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
+/*
+let notes = [
+  {
+    id: "1",
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: "2",
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }
+]*/
--- osa3/puhelinluettelo/back/index.js
+++ osa3/puhelinluettelo/back/index.js
@@ -11,37 +11,63 @@ morgan.token('body', (request) => JSON.stringify(request.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 
-app.get('/', (request, response) => {
-  response.send('Server is running!')
-})
-
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response, next) => {
     Person.find({}).then( persons => {
         response.json(persons)
     })
+    .catch(error => next(error))
 })
 
-app.get('/info', (request, response) => {
+app.get('/info', (request, response, next) => {
     request.requestTime = new Date()
-    response.send(`<div><p>Phonebook has info for ${persons.length} people</p><p></p><p>${request.requestTime}`)
+    Person.find({}).then( persons => {
+        response.send(`<div><p>Phonebook has info for ${persons.length} people</p><p></p><p>${request.requestTime}`)
+    
+    })
+    .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     const id = request.params.id
 
     Person.findById(id).then( person => {
-        response.json(person)
+        if (person) {
+            response.json(person)
+        } else {
+            response.status(404).end()
+        }
     })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     const id = request.params.id
-    persons = persons.filter(p=> p.id !== id)
-    response.status(204).end()
+    Person.findByIdAndDelete(id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+    const { name, number } = request.body
 
-app.post('/api/persons', (request, response) => {
+    Person.findById(request.params.id)
+        .then(person => {
+        if (!person) {
+            return response.status(404).end()
+        }
+
+        person.number = number
+
+        return person.save().then((updatedPerson) => {
+            response.json(updatedPerson)
+        })
+        })
+        .catch(error => next(error))
+})
+
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
     if (!body.name){
@@ -70,7 +96,26 @@ app.post('/api/persons', (request, response) => {
     person.save().then( savedPerson => {
         response.json(savedPerson)
     })
+    .catch(error => next(error))
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = 3001
 app.listen(PORT, () => {
