9bb4e6d4c0718277c5e4a06f0f8006c30bd6ffe6
markusmuilu <muilu.markus@gmail.com>
2025-09-17 16:35:19 +0300

    removed additional header anecdotes

--- osa6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ osa6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -8,7 +8,6 @@ const Anecdotes = () => {
 
   return(
     <div>
-      <h2>Anecdotes</h2>
       {anecdotes.sort((a,b)=> b.votes - a.votes).map(anecdote =>
         <div key={anecdote.id}>
           <div>
