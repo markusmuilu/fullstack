e025b3f558fd1ed66ef9b4f7ae2ec3439e60dc8c
markusmuilu <muilu.markus@gmail.com>
2025-09-17 15:05:07 +0300

    6.1-6.2

---
+++ osa6/redux/src/main.jsx
@@ -0,0 +1,12 @@
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    case 'ZERO':
+      return 0
+    default: // jos ei mikään ylläolevista tullaan tänne
+      return state
+  }
+}
